a681dcbe0fd57cdc9ae00f2ddf206d27b9d40be7
Ivan Cabrera <ivangonzalocabrera@gmail.com>
2024-10-23 17:54:12 -0300

    Ultima modificacion, se agrego un comentario

--- src/test/java/com/example/EquipoTest.java
+++ src/test/java/com/example/EquipoTest.java
@@ -1,7 +1,6 @@
 package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
 
 import java.util.ArrayList;
 import java.time.LocalDate;
